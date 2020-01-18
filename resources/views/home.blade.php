ecc1799b5cf09011e674ff692696fcaf2898226b
RD <romualdas@prodev.lt>
2020-01-18 16:55:52 +0200

    Ad filters

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,5 @@
 
 @section('content')
     @include('partials.categories')
+    @include('partials.ad-filters')
 @endsection
---
+++ resources/views/partials/ad-filters.blade.php
@@ -0,0 +1,25 @@
+<div class="container">
+    <div class="flex flex-col lg:flex-row lg:items-center justify-between">
+        <div class="flex justify-between lg:text-18px font-light">
+            Rodomi verslo skelbimai <span class="text-blue-3 font-normal ml-1">(12)</span>
+        </div>
+        <div class="flex flex-col-reverse lg:flex-row lg:items-center">
+            <div class="flex justify-between pb-20px lg:pb-0">
+                <label class="flex items-center lg:px-4 text-12px font-light">
+                    <input type="checkbox" class="form-checkbox mr-3 h-6 w-6">
+                    Fizinis verslas
+                </label>
+                <label class="flex items-center px-4 text-12px font-light">
+                    <input type="checkbox" class="form-checkbox mr-3 h-6 w-6">
+                    Internetinis verslas
+                </label>
+            </div>
+            <div class="lg:pl-6 py-20px lg:py-10px">
+                <select class="form-select block w-full text-12px pl-4 py-14px mr-8">
+                    <option>Naujausi viršuje</option>
+                    <option>Seniausi viršuje</option>
+                </select>
+            </div>
+        </div>
+    </div>
+</div>
