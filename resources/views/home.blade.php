db2f56fdb98d936291ae5bbe50ee00d5500590b8
RD <romualdas@prodev.lt>
2020-01-18 17:15:18 +0200

    Footer cities

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,41 +3,5 @@
 @section('content')
     @include('partials.categories')
     @include('partials.ad-filters')
-    <div class="container py-20px">
-        <h2 class="text-24px font-light mb-8">Verslo skelbimai Lietuvos miestuose</h2>
-        <ul class="flex flex-wrap -mx-4">
-            <li class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 px-4 text-14px font-light mb-4"><a href="#">Vilnius <span class="text-purple-1">(3,582)</span></a></li>
-            <li class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 px-4 text-14px font-light mb-4"><a href="#">Kaunas <span class="text-purple-1">(3,582)</span></a></li>
-            <li class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 px-4 text-14px font-light mb-4"><a href="#">Klaipėda <span class="text-purple-1">(3,582)</span></a></li>
-            <li class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 px-4 text-14px font-light mb-4"><a href="#">Šiauliai <span class="text-purple-1">(3,582)</span></a></li>
-            <li class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 px-4 text-14px font-light mb-4"><a href="#">Panevėžys <span class="text-purple-1">(3,582)</span></a></li>
-            <li class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 px-4 text-14px font-light mb-4"><a href="#">Alytus <span class="text-purple-1">(3,582)</span></a></li>
-            <li class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 px-4 text-14px font-light mb-4"><a href="#">Biržai <span class="text-purple-1">(3,582)</span></a></li>
-            <li class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 px-4 text-14px font-light mb-4"><a href="#">Druskininkai <span class="text-purple-1">(3,582)</span></a></li>
-            <li class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 px-4 text-14px font-light mb-4"><a href="#">Elektrėnai <span class="text-purple-1">(3,582)</span></a></li>
-            <li class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 px-4 text-14px font-light mb-4"><a href="#">Gargždai <span class="text-purple-1">(3,582)</span></a></li>
-            <li class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 px-4 text-14px font-light mb-4"><a href="#">Garliava <span class="text-purple-1">(3,582)</span></a></li>
-            <li class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 px-4 text-14px font-light mb-4"><a href="#">Jonava <span class="text-purple-1">(3,582)</span></a></li>
-            <li class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 px-4 text-14px font-light mb-4"><a href="#">Kėdainiai <span class="text-purple-1">(3,582)</span></a></li>
-            <li class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 px-4 text-14px font-light mb-4"><a href="#">Kretinga <span class="text-purple-1">(3,582)</span></a></li>
-            <li class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 px-4 text-14px font-light mb-4"><a href="#">Kuršėnai <span class="text-purple-1">(3,582)</span></a></li>
-            <li class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 px-4 text-14px font-light mb-4"><a href="#">Marijampolė <span class="text-purple-1">(3,582)</span></a></li>
-            <li class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 px-4 text-14px font-light mb-4"><a href="#">Mažeikiai <span class="text-purple-1">(3,582)</span></a></li>
-            <li class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 px-4 text-14px font-light mb-4"><a href="#">Neringa <span class="text-purple-1">(3,582)</span></a></li>
-            <li class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 px-4 text-14px font-light mb-4"><a href="#">Palanga <span class="text-purple-1">(3,582)</span></a></li>
-            <li class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 px-4 text-14px font-light mb-4"><a href="#">Plungė <span class="text-purple-1">(3,582)</span></a></li>
-            <li class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 px-4 text-14px font-light mb-4"><a href="#">Prienai <span class="text-purple-1">(3,582)</span></a></li>
-            <li class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 px-4 text-14px font-light mb-4"><a href="#">Radviliškis <span class="text-purple-1">(3,582)</span></a></li>
-            <li class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 px-4 text-14px font-light mb-4"><a href="#">Rokiškis <span class="text-purple-1">(3,582)</span></a></li>
-            <li class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 px-4 text-14px font-light mb-4"><a href="#">Šilutė <span class="text-purple-1">(3,582)</span></a></li>
-            <li class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 px-4 text-14px font-light mb-4"><a href="#">Tauragė <span class="text-purple-1">(3,582)</span></a></li>
-            <li class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 px-4 text-14px font-light mb-4"><a href="#">Telšiai <span class="text-purple-1">(3,582)</span></a></li>
-            <li class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 px-4 text-14px font-light mb-4"><a href="#">Trakai <span class="text-purple-1">(3,582)</span></a></li>
-            <li class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 px-4 text-14px font-light mb-4"><a href="#">Ukmergė <span class="text-purple-1">(3,582)</span></a></li>
-            <li class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 px-4 text-14px font-light mb-4"><a href="#">Utena <span class="text-purple-1">(3,582)</span></a></li>
-            <li class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 px-4 text-14px font-light mb-4"><a href="#">Visaginas <span class="text-purple-1">(3,582)</span></a></li>
-            <li class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 px-4 text-14px font-light mb-4"><a href="#">Zarasai <span class="text-purple-1">(3,582)</span></a></li>
-            <li class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 px-4 text-14px font-light mb-4"><a href="#">Kitas <span class="text-purple-1">(3,582)</span></a></li>
-        </ul>
-    </div>
+    @include('partials.footer-cities')
 @endsection
