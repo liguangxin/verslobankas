3d4f1146e45a14402533566bd7163307d766c8d4
RD <romualdas@prodev.lt>
2020-02-12 09:25:16 +0200

    Perziureti skelbimai

--- resources/views/perziureti-skelbimai.blade.php
+++ resources/views/perziureti-skelbimai.blade.php
@@ -4,6 +4,9 @@
 
 @section('content')
     <div class="container py-50px">
+        <div class="lg:text-18px font-light mb-6">
+            Jūsų peržiūrėtų skelbimų sąrašas (iki 100 skelbimų)
+        </div>
         @repeat(3)
             @include('partials.ad-perziureti')
         @endrepeat
